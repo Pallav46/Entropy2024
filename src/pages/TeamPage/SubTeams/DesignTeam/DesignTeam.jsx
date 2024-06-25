85a0e1a70365e57c8a8762711ef2a9799bc95ec8
NISHANT <iampruh887@gmail.com>
2024-06-25 16:05:39 +0530

    reverted back.

--- src/pages/TeamPage/SubTeams/DesignTeam/DesignTeam.jsx
+++ src/pages/TeamPage/SubTeams/DesignTeam/DesignTeam.jsx
@@ -1,75 +1,8 @@
-import React, { useState } from 'react';
-import './DesignTeam.css';
-import Sparkles from './sparkles'; // Adjust path as needed based on your project structure
 
-const teamMembers = [
-  {
-    name: 'Alex',
-    role: 'Designer',
-    bio: 'Expert in frontend development.',
-    favoriteGame: 'Cyberpunk 2077',
-    image: '/public/images/design/alex.jpg'
-  },
-  {
-    name: 'Steve',
-    role: 'Developer',
-    bio: 'Passionate about low level programming.',
-    favoriteGame: 'Overwatch',
-    image: '/public/images/design/steve.png'
-  },
-  {
-    name: 'Wither Skeleton',
-    role: 'Tester',
-    bio: 'Self proclaimed expert in judging others work.',
-    favoriteGame: 'D&D',
-    image: '/public/images/design/wither_skeleton.jpg'
-  },
-  {
-    name: 'Herobrine',
-    role: 'The Intern',
-    bio: 'I deleted the production database twice',
-    favoriteGame: 'Asphalt',
-    image: '/public/images/design/herobrine.png'
-  }
-  // Additional team members
-];
-
-const DesignTeam = () => {
-  const [selectedMember, setSelectedMember] = useState(null);
-
+export default function DesignTeam() {
   return (
-    <div className='page-bg'>
-      <div className="design-team">
-        <h1>Meet Our Team</h1>
-        <div className="team-grid">
-          {teamMembers.map((member, index) => (
-            <div 
-              key={index} 
-              className="team-member" 
-              onClick={() => setSelectedMember(member)}
-            >
-              <img src={member.image} alt={member.name} />
-              <h2>{member.name}</h2>
-            </div>
-          ))}
-        </div>
-
-        {selectedMember && (
-          
-            <div className="member-details">
-              <div className="card">
-                <img src={selectedMember.image} />
-                <h2><Sparkles>{selectedMember.name}</Sparkles></h2>
-                <h3><Sparkles>{selectedMember.role}</Sparkles></h3>
-                <p><Sparkles>{selectedMember.bio}</Sparkles></p>
-                <p><Sparkles><strong>Favorite Game:</strong> {selectedMember.favoriteGame}</Sparkles></p>
-                <button onClick={() => setSelectedMember(null)}>Close</button>
-              </div>
-            </div>
-        )}
-      </div>
+    <div>
+      DesignTeam
     </div>
-  );
-};
-
-export default DesignTeam;
+  )
+}
