206e378a7163435ae6d3ac78e16f64093b9f2559
1806PiYuSh1806 <piyushupadhyay37756@gmail.com>
2024-06-26 16:39:31 +0530

    Added Navbar component. It's just for dummy purpose. It needs more work

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import classes from "./TeamPage.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Navbar from "../../components/Navbar/Navbar";
 
 class WebAndCreatives extends React.Component {
   render() {
@@ -20,106 +21,107 @@ class WebAndCreatives extends React.Component {
     };
 
     return (
-      <div className={classes.main_container}>
-        <div className={classes.blogSlider}>
-          <Slider {...settings}>
-            <div className={classes.blogSliderItem}>
-              <div className={classes.blogSliderImg}>
-                <img
-                  src="/assets/TeamPage/FacultyCoordinators.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={classes.blogSliderContent}>
-                <div className={classes.blogSliderTitle}>
-                  Faculty Coordinators
+      <>
+        <Navbar/>
+        <div className={classes.main_container}>
+          <div className={classes.blogSlider}>
+            <Slider {...settings}>
+              <div className={classes.blogSliderItem}>
+                <div className={classes.blogSliderImg}>
+                  <img src="/assets/TeamPage/FacultyCoordinators.jpg" alt="" />
                 </div>
-                <div className={classes.blogSliderText}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
+                <div className={classes.blogSliderContent}>
+                  <div className={classes.blogSliderTitle}>
+                    Faculty Coordinators
+                  </div>
+                  <div className={classes.blogSliderText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a href="/teams/faculty" className={classes.blogSliderButton}>
+                    MEET THE TEAM
+                  </a>
                 </div>
-                <a href="/teams/faculty" className={classes.blogSliderButton}>
-                  MEET THE TEAM
-                </a>
               </div>
-            </div>
-            <div className={classes.blogSliderItem}>
-              <div className={classes.blogSliderImg}>
-                <img
-                  src="/assets/TeamPage/SponsorshipTeam.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={classes.blogSliderContent}>
-                <div className={classes.blogSliderTitle}>
-                  Sponsorship Team
+              <div className={classes.blogSliderItem}>
+                <div className={classes.blogSliderImg}>
+                  <img src="/assets/TeamPage/SponsorshipTeam.jpg" alt="" />
                 </div>
-                <div className={classes.blogSliderText}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
+                <div className={classes.blogSliderContent}>
+                  <div className={classes.blogSliderTitle}>
+                    Sponsorship Team
+                  </div>
+                  <div className={classes.blogSliderText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a
+                    href="/teams/sponsorship"
+                    className={classes.blogSliderButton}
+                  >
+                    MEET THE TEAM
+                  </a>
                 </div>
-                <a href="/teams/sponsorship" className={classes.blogSliderButton}>
-                  MEET THE TEAM
-                </a>
               </div>
-            </div>
-            <div className={classes.blogSliderItem}>
-              <div className={classes.blogSliderImg}>
-                <img
-                  src="/assets/TeamPage/WebAndCreatives.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={classes.blogSliderContent}>
-                <div className={classes.blogSliderTitle}>Web And Creatives</div>
-                <div className={classes.blogSliderText}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
+              <div className={classes.blogSliderItem}>
+                <div className={classes.blogSliderImg}>
+                  <img src="/assets/TeamPage/WebAndCreatives.jpg" alt="" />
                 </div>
-                <a href="/teams/webAndCreatives" className={classes.blogSliderButton}>
-                  MEET THE TEAM
-                </a>
-              </div>
-            </div>
-            <div className={classes.blogSliderItem}>
-              <div className={classes.blogSliderImg}>
-                <img
-                  src="/assets/TeamPage/EventManagement.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={classes.blogSliderContent}>
-                <div className={classes.blogSliderTitle}>Event Management</div>
-                <div className={classes.blogSliderText}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
+                <div className={classes.blogSliderContent}>
+                  <div className={classes.blogSliderTitle}>
+                    Web And Creatives
+                  </div>
+                  <div className={classes.blogSliderText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a
+                    href="/teams/webAndCreatives"
+                    className={classes.blogSliderButton}
+                  >
+                    MEET THE TEAM
+                  </a>
                 </div>
-                <a href="/teams/eventManagement" className={classes.blogSliderButton}>
-                  MEET THE TEAM
-                </a>
               </div>
-            </div>
-            <div className={classes.blogSliderItem}>
-              <div className={classes.blogSliderImg}>
-                <img
-                  src="/assets/TeamPage/DesignTeam.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={classes.blogSliderContent}>
-                <div className={classes.blogSliderTitle}>Design Team</div>
-                <div className={classes.blogSliderText}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Recusandae voluptate repellendus magni illo ea animi?
+              <div className={classes.blogSliderItem}>
+                <div className={classes.blogSliderImg}>
+                  <img src="/assets/TeamPage/EventManagement.jpg" alt="" />
                 </div>
-                <a href="/teams/design" className={classes.blogSliderButton}>
-                  MEET THE TEAM
-                </a>
+                <div className={classes.blogSliderContent}>
+                  <div className={classes.blogSliderTitle}>
+                    Event Management
+                  </div>
+                  <div className={classes.blogSliderText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a
+                    href="/teams/eventManagement"
+                    className={classes.blogSliderButton}
+                  >
+                    MEET THE TEAM
+                  </a>
+                </div>
               </div>
-            </div>
-          </Slider>
+              <div className={classes.blogSliderItem}>
+                <div className={classes.blogSliderImg}>
+                  <img src="/assets/TeamPage/DesignTeam.jpg" alt="" />
+                </div>
+                <div className={classes.blogSliderContent}>
+                  <div className={classes.blogSliderTitle}>Design Team</div>
+                  <div className={classes.blogSliderText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a href="/teams/design" className={classes.blogSliderButton}>
+                    MEET THE TEAM
+                  </a>
+                </div>
+              </div>
+            </Slider>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
