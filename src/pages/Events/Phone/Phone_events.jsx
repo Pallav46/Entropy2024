46f7fac3cdbb796d53fa97f567a54c092e89a994
Shouraya16 <shouraya.mishra1604@gmail.com>
2024-07-02 15:56:37 +0530

    Made separate folders for mobile and laptop and configured them to work in the website

---
+++ src/pages/Events/Phone/Phone_events.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Phone_events.css';
+
+const Phone_events = () => {
+  return (
+    <div id='main'>
+        Phone events page
+    </div>
+  )
+}
+
+export default Phone_events
