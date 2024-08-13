3713bc9aafa71735648da3ec4cd46b651a33e2d5
Prithvi Raj <rprithvi939@gmail.com>
2024-08-14 02:34:47 +0530

    uploaded "Trailer & Cursor" file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Trailer from './pages/Home/Trailer/Trailer'
+import Cursor from './pages/Home/Cursor/Cursor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Entropy 2024</h1>
+      <Cursor/>
+      <Trailer/>
     </>
   )
 }
