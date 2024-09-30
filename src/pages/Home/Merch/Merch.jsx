a42ffb21cc8c4864c1665404b4f6b3de09bc41c9
Diya-16204 <151431840+Diya-16204@users.noreply.github.com>
2024-09-30 17:47:36 +0530

    Merch JSX File Updated

--- src/pages/Home/Merch/Merch.jsx
+++ src/pages/Home/Merch/Merch.jsx
@@ -1,228 +1,36 @@
-import { useEffect, useRef } from 'react';
-import './Merch.css';
-
+import './Merch.css'
 const Merch = () => {
-    const nextBtnRef = useRef(null);
-    const prevBtnRef = useRef(null);
-    const carouselRef = useRef(null);
-    const listRef = useRef(null);
-    const runningTimeRef = useRef(null);
+  return (
+    <div>
+    <header className="header">
+      <a href="#home" className="logo">IIITG <span> Entropy</span></a>
 
-    const timeRunning = 3000;
-    const timeAutoNext = 7000;
-    let runTimeOut;
-    let runNextAuto;
+      <i className='bx bx-menu' id="menu-icon"></i>
 
-    useEffect(() => {
-        const nextBtn = nextBtnRef.current;
-        const prevBtn = prevBtnRef.current;
-        const carousel = carouselRef.current;
-        const list = listRef.current;
-        const runningTime = runningTimeRef.current;
+    </header>
 
-        const showSlider = (type) => {
-            const sliderItemsDom = list.querySelectorAll('.mr-item');
-            if (type === 'mr-next') {
-                list.appendChild(sliderItemsDom[0]);
-                carousel.classList.add('mr-next');
-            } else {
-                list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
-                carousel.classList.add('mr-prev');
-            }
+    <section className="home" id="home">
+      <div className="home-content">
+        <h1> 2024<span> Collection</span></h1>
+        <h3 className="text-animation">Elevate your Style with
 
-            clearTimeout(runTimeOut);
+Techniche Merch! <span></span></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus voluptatum itaque assumenda quas aspernatur sit non consequuntur libero earum hic vitae, quos ab fuga ut nesciunt eos, at deserunt.        </p>
+        <br></br>
+        <br></br>
 
-            runTimeOut = setTimeout(() => {
-                carousel.classList.remove('mr-next');
-                carousel.classList.remove('mr-prev');
-            }, timeRunning);
 
-            clearTimeout(runNextAuto);
-            runNextAuto = setTimeout(() => {
-                nextBtn.click();
-            }, timeAutoNext);
 
-            resetTimeAnimation();
-        };
-
-        const resetTimeAnimation = () => {
-            runningTime.style.animation = 'none';
-            runningTime.offsetHeight; /* trigger reflow */
-            runningTime.style.animation = null;
-            runningTime.style.animation = 'runningTime 7s linear 1 forwards';
-        };
-
-        nextBtn.onclick = () => {
-            showSlider('mr-next');
-        };
-
-        prevBtn.onclick = () => {
-            showSlider('mr-prev');
-        };
-
-        runNextAuto = setTimeout(() => {
-            nextBtn.click();
-        }, timeAutoNext);
-
-        // Start the initial animation
-        resetTimeAnimation();
-
-        // Clean up the timers when the component is unmounted
-        return () => {
-            clearTimeout(runTimeOut);
-            clearTimeout(runNextAuto);
-        };
-    }, []);
-
-    return (
-        <div className='mr-container'>
-
-            <div className="mr-carousel" ref={carouselRef}>
-                <div className="mr-list" ref={listRef}>
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/eagel1.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">EAGLE</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/owl1.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">OWL</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/crow.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">CROW</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/butterfly1.jpeg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">BUTTERFLY</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/owl2.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">OWL</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/eagel3.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">EAGLE</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/kingfirser2.jpeg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">KINGFISHER</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/parrot2.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">PARROT</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/heron.jpeg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">HERON</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/butterfly2.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">BUTTERFLY</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mr-item" style={{ backgroundImage: "url(/image/parrot2.jpg)" }}>
-                        <div className="mr-content">
-                            <div className="mr-title">SLIDER</div>
-                            <div className="mr-name">PARROT</div>
-                            <div className="mr-des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
-                            <div className="mr-btn">
-                                <button>See More</button>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* next prev button */}
-                <div className="mr-arrows">
-                    <button className="mr-prev" ref={prevBtnRef}>&lt;</button>
-                    <button className="mr-next" ref={nextBtnRef}>&gt;</button>
-                </div>
-
-                {/* time running */}
-                <div className="mr-timeRunning" ref={runningTimeRef}></div>
-            </div>
+        <div className="btn-group">
+          <a href="#contact" className="btn">Buy Now</a>
         </div>
-    );
-};
+      </div>
+      <div className="home-img">
+        <img src="https://s3.us-east-1.amazonaws.com/cdn.designcrowd.com/blog/merchandise-designs-for-every-day-branding/T-shirt-Design-by-Risallah.png" alt="" />
+      </div>
+    </section>
+  </div>
+  )
+}
 
 export default Merch;
